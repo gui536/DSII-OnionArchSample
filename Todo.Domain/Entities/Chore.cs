bfc2eef4c0298e1f8e5ac240a60d864ab46a0240
Guilherme Santos <guilherme.santos536@etec.sp.gov.br>
2024-09-25 21:03:12 -0300

    feat: created project folder structure

---
+++ Todo.Domain/Entities/Chore.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+using Todo.Domain.Validation;
+
+namespace Todo.Domain.Entities
+{
+    sealed class Chore : Entity
+    {
+        private int id;
+        private string name;
+        private string description;
+        private DateTime dueDate;
+        private Status status;
+        private User assignedTo;
+
+        private void ValidateDomain(string name, string description, 
+            DateTime dueDate, Status status,
+            User assignedTo)
+        {
+            DomainExcepetionValidation.When(name.Length == 0, "Nome não pode ser vazio");
+            DomainExcepetionValidation.When(dueDate.Year > DateTime.Now.AddYears(3).Year, 
+                "Não podem ser criadas tarefas para depois de três anos");
+        }
+    }
+
+
+}
---
+++ Todo.Domain/Validation/DomainExcepetionValidation.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Todo.Domain.Validation
+{
+    public class DomainExcepetionValidation : Exception
+    {
+        public DomainExcepetionValidation(string error) : base(error) {
+        }
+
+        public static void When(bool hasError, string error)
+        {
+            if (hasError)
+                throw new DomainExcepetionValidation(error);
+        }
+    }
+}

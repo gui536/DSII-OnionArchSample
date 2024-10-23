87797fd46a236fd79760e2fb72238f8f291548f5
Guilherme Santos <guilherme.santos536@etec.sp.gov.br>
2024-10-23 13:20:05 -0300

    feat: Added RepositoryClasses

---
+++ Persistance/Repositories/ChoreRepository.cs
@@ -0,0 +1,58 @@
+﻿using Domain.Entities;
+using Domain.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using Persistance.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistance.Repositories
+{
+    public class ChoreRepository : IChoreRepository
+    {
+        private ApplicationDbContext _choreContext;
+
+        public ChoreRepository(ApplicationDbContext choreContext)
+        {
+            _choreContext = choreContext;
+        }
+
+        public async Task DeleteAsync(Chore chore)
+        {
+            _choreContext.Remove(chore);
+            await _choreContext.SaveChangesAsync();
+        }
+
+        public async Task<IEnumerable<Chore?>> GetAll()
+        {
+            return await _choreContext.Chores.ToListAsync<Chore>();
+        }
+
+        public async Task<IEnumerable<Chore?>> GetAllByUserId(int? userId)
+        {
+            return await _choreContext.Chores
+                .Where(chore => chore.AssignedToId == userId)
+                .ToListAsync();
+        }
+
+        public async Task<Chore?> GetByIdAsync(int? id)
+        {
+            return await _choreContext.Chores.FindAsync(id);
+        }
+
+        public async Task<int> InsertAsync(Chore chore)
+        {
+            _choreContext.Add(chore);
+            var result = await _choreContext.SaveChangesAsync();
+            return chore.Id;
+        }
+
+        public async Task UpdateAsync(Chore chore)
+        {
+            _choreContext.Update(chore);
+            await _choreContext.SaveChangesAsync();
+        }
+    }
+}
---
+++ Persistance/Repositories/UserRepository.cs
@@ -0,0 +1,51 @@
+﻿using Domain.Entities;
+using Domain.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using Persistance.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistance.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        private ApplicationDbContext _userContext;
+
+        public UserRepository(ApplicationDbContext userContext)
+        {
+            _userContext = userContext;
+        }
+
+        public async Task DeleteAsync(User user)
+        {
+            _userContext.Remove(user);
+            await _userContext.SaveChangesAsync();
+        }
+
+        public async Task<IEnumerable<User?>> GetAll()
+        {
+            return await _userContext.Users.ToListAsync<User>();
+        }
+
+        public async Task<User?> GetByIdAsync(int? id)
+        {
+            return await _userContext.Users.FindAsync(id);
+        }
+
+        public async Task UpdateAsync(User user)
+        {
+            _userContext.Update(user);
+            await _userContext.SaveChangesAsync();
+        }
+        
+        public async Task<int>InsertAsync(User user)
+        {
+            _userContext.Add(user);
+            await _userContext.SaveChangesAsync();
+            return user.Id;
+        }
+    }
+}
